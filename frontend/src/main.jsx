dd3d0fd5d81f2bc949a8ea5156963d2a203fedd3
enibalo <abibalo@hotmail.com>
2024-11-13 01:08:33 -0700

    more set up

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './styles/index.css'
-import App from './views/App.jsx'
+import Login from './views/Login.jsx'
+import UserSettings from './views/UserSettings.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}></Route>
+      <Route path="/settings" element={<UserSettings/>}></Route>
+      <Route path="/signup" element={<SignUp/>}></Route>
+      <Route path="/search" element={<Search/>}></Route>
+      <Route path="/book" element={<Book/>}></Route>
+      <Route path="/addBook" element={<AddBook/>}></Route>
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ frontend/src/views/Book.jsx
@@ -0,0 +1,12 @@
+
+import { useState } from "react";
+import { useEffect } from "react";
+import {Header} from '../components/Header.jsx'
+
+export default function Book(){
+    return(
+        <>
+        <Header></Header>
+        </>
+    )
+}
