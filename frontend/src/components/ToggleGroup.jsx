5180a3c85b76e5b959e9c1943ae3b301612ac73e
enibalo <abibalo@hotmail.com>
2024-11-19 12:31:29 -0700

    add template form

--- frontend/src/components/ToggleGroup.jsx
+++ frontend/src/components/ToggleGroup.jsx
@@ -1,10 +1,10 @@
-import styles from "../../styles/Book.module.css";
 import { useFormContext } from "react-hook-form";
 import PropTypes from "prop-types";
-import check from "../../assets/Check.svg";
+import check from "../assets/Check.svg";
 
 // not done styling it!!
-//Also, any forms that implement it must use react-hook-form ( made it easy to make sure that at least one item is selected)
+//Also, any forms that implement it must use react-hook-form (it made it easy to make sure that at least one item is selected)
+//Template form is at the bottom of the file.
 export default function ToggleGroup({ items, itemName }) {
   console.log(items);
   const {
@@ -61,3 +61,30 @@ ToggleGroup.propTypes = {
   items: PropTypes.array,
   itemName: PropTypes.string,
 };
+
+/**
+//The template for forms which use, ToggleGroup. 
+
+import { useForm, FormProvider} from "react-hook-form";
+
+export default function DummyForm() {
+  const methods = useForm();
+
+  // items is a list of what should be displayed in toggle group 
+  // itemName is the name of the toggle group. like Tags, or Genres .
+
+  const onSubmit = (data) => {
+    //whatever you do with form data 
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <ToggleGroup items={items} itemName={"ToggleGroupName"}></ToggleGroup>
+          <input type="submit"></input>
+      </form>
+    </FormProvider>
+  );
+}
+
+ */
