22245084fbac5dbd31332ad0feda1f15821367b0
enibalo <abibalo@hotmail.com>
2024-11-19 18:33:33 -0700

    clean up code

--- frontend/src/views/BookComponents/AddRec.jsx
+++ frontend/src/views/BookComponents/AddRec.jsx
@@ -2,15 +2,13 @@ import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 import styles from "../../styles/Book.module.css";
-import { useForm, FormProvider, useFormContext } from "react-hook-form";
-import PropTypes from "prop-types";
-import check from "../../assets/Check.svg";
+import { useForm, FormProvider } from "react-hook-form";
+import ToggleGroup from "../../components/ToggleGroup.jsx";
 
 export default function AddRec() {
   const methods = useForm({
     defaultValues: {
-      comment:
-        "This book has the same tropes, and it&#39;s really well written!",
+      comment: `This book has the same tropes, and it's really well written!`,
     },
   });
 
@@ -74,114 +72,3 @@ async function fetchTags() {
   let tags = ["fun", "cool", "awesome"];
   return tags;
 }
-
-export function ToggleGroup({ items, itemName }) {
-  const {
-    register,
-    formState: { errors },
-  } = useFormContext();
-
-  function atLeastOneChecked(checkboxes) {
-    return checkboxes.length > 0;
-  }
-
-  return (
-    <fieldset className={"toggleGroup"}>
-      <legend>Select Your {itemName}</legend>
-      {items.map((item, index) => {
-        return (
-          <label
-            htmlFor={"input-" + item}
-            aria-labelledby={"span-" + item}
-            key={item}
-          >
-            {index == 0 ? (
-              <input
-                type="checkbox"
-                id={"input-" + item}
-                name="toggleGroup"
-                value={item}
-                {...register("checkbox", {
-                  validate: atLeastOneChecked,
-                })}
-              ></input>
-            ) : (
-              <input
-                type="checkbox"
-                id={"input-" + item}
-                name="toggleGroup"
-                value={item}
-                {...register("checkbox")}
-              ></input>
-            )}
-
-            <span
-              className={styles.center}
-              aria-hidden="true"
-              tabIndex="-1"
-              id={"span-" + item}
-            >
-              <img src={check}></img>
-              <div className="inner-text">{item}</div>
-            </span>
-          </label>
-        );
-      })}
-      {errors.checkbox && (
-        <div className="error">You must select at least one item.</div>
-      )}
-    </fieldset>
-  );
-}
-
-ToggleGroup.propTypes = {
-  items: PropTypes.array,
-  itemName: PropTypes.string,
-};
-
-/*
-
-  <label for="work_day_sun" aria-labelledby="sun">
-    <input type="checkbox" id="work_day_sun" name="work_days" value="1" {...register("chechbox", {
-     validate: atLeastOneChecked,
-  })}> 
-    <span aria-hidden="true" tabIndex="-1" id="sun">Sunday</span>
-  </label>
-
-  //all should be registered as same name, validate only needed on one of them 
-
-  function atLeastOneMediumChecked(selectedMediums: string[]) {
-  return selectedMediums.length > 0;
-}
-
-{errors.medium && (
-  <span>
-    At least one medium selection is required
-  </span>
-)}
-
-<label><input type="checkbox" name="work_days" value="1"><span>sun</span></label>
-  <label><input type="checkbox" name="work_days" value="2"><span>mon</span></label>
-  <label><input type="checkbox" name="work_days" value="3"><span>tue</span></label>
-  <label><input type="checkbox" name="work_days" value="4"><span>wed</span></label>
-  <label><input type="checkbox" name="work_days" value="5"><span>thu</span></label>
-  <label><input type="checkbox" name="work_days" value="6"><span>fri</span></label>
-
-  #id_work_days input[type="checkbox"] {
-  display: none;
-}
-
-#id_work_days span {
-  display: inline-block;
-  padding: 10px;
-  text-transform: uppercase;
-  border: 2px solid gold;
-  border-radius: 3px;
-  color: gold;
-}
-
-#id_work_days input[type="checkbox"]:checked + span {
-  background-color: gold;
-  color: black;
-}
-*/
