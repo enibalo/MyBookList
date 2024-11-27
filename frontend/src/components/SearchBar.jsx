df5db48e0c8af84c3583ee58efec7a56724ae92d
Anshi <anshikhatri37@gmail.com>
2024-11-26 20:05:41 -0700

    Updated Search Bar
    Updated the buttons for the search bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,18 +1,39 @@
 
 
 import styles from "../styles/SearchBar.module.css";
+import  MenuIcon from "../assets/Menu.svg";
+import  SearchIcon  from "../assets/Magnifying-Glass.svg";
 
 function SearchBar() {
   return (
     <div className={styles["header-container"]}>
-      <button className={styles["menu-icon"]}>☰</button>
+      <button className={styles["menu-icon"]}>
+      <img
+                  alt="Menu Icon"
+                  aria-label="User Pressed Menu"
+                  className={styles.icon}
+                  src={MenuIcon}
+                  width={15}
+                  height={15}
+                ></img>
+        
+      </button>
       <div className={styles["search-bar"]}>
         <input
           type="text"
           className={styles["search-input"]}
           placeholder="Hinted search text"
         />
-        <button className={styles["search-icon"]} aria-label ="Search Button"> 🔍 </button>
+        <button className={styles["search-icon"]} aria-label ="Search Button"> 
+        <img
+                  alt="Search Icon"
+                  aria-label="User Pressed Search"
+                  className={styles.icon}
+                  src={SearchIcon}
+                  width={15}
+                  height={15}
+                ></img>
+        </button>
       </div>
     </div>
   );
